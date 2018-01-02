5c9de388ee73c1d06f4761c0e6c74b95c9a3a211
DimaGashko <dgashko@mail.ua>
2018-01-02 18:14:35 +0300

    main.js - переключение с секундомера на таймер

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,37 @@
 ﻿;(function($){
 	"use strict"
 	
+	var els = {
+		openHelp: document.querySelector('.header__help'),
+		openSet: document.querySelector('.header__set'),
+		
+		selectStopwatch: document.querySelector('.iTimer__selectStopwatch'),
+		selectTimer: document.querySelector('.iTimer__selectTimer'),
+		
+		iTimer: document.querySelector('.iTimer'),
+		stopwatch: document.querySelector('.iTimer__stopwatch'),
+		timer: document.querySelector('.iTimer__timer'),
+	}
 	
+	//help
+	els.openHelp.addEventListener('click', () => {
+		alert('Помощь будет добавлена в скором времени!');
+	});
 	
-}());
+	//set
+	els.openSet.addEventListener('click', () => {
+		alert('В данный момент настройки недоступны.');
+	});
+	
+	//select type iTimer	
+	els.selectStopwatch.addEventListener('click', () => {
+		els.iTimer.classList.remove('iTimer-timer');
+		els.iTimer.classList.add('iTimer-stopwatch');
+	});
+	
+	els.selectTimer.addEventListener('click', () => {
+		els.iTimer.classList.remove('iTimer-stopwatch');
+		els.iTimer.classList.add('iTimer-timer');
+	});
+	
+}(jQuery));
